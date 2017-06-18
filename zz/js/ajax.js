c7cd5e23dd870f530463e8719dd658d711f0127e
liudewang <757566917@qq.com>
2017-06-18 19:33:11 +0800

    pp
    pp

--- zz/js/ajax.js
+++ zz/js/ajax.js
@@ -2,17 +2,23 @@ $(function()
 {
 	var TpArr=[];
 	var DzArr=[];
-		 		var dz="http://m.neihanshequ.com/?is_json=1&app_name=neihanshequ_video&min_time=1497699938&csrfmiddlewaretoken=70faddc0d620ee6ba01b2e0627593360";
+		 		var dz="http://proxy.e12e.com/?http://m.neihanshequ.com/?is_json=1&skip_guidence=1;
 		 		var	tp="http://m.neihanshequ.com/pic/?is_json=1&app_name=neihanshequ_web&min_time=1497695422&csrfmiddlewaretoken=70faddc0d620ee6ba01b2e0627593360";
-function ajax(attr,t)
 
 //var vido="http://m.neihanshequ.com/video/?is_json=1&app_name=neihanshequ_web&min_time=1497758177&csrfmiddlewaretoken=70faddc0d620ee6ba01b2e0627593360";
+	var y=0;
+	var my=0;
+	var top=0;
+function ajax(attr,t)
 {
+	function xr(i)
+	{
+			ul.innerHTML+="<li>"+"<h1>"+"<img src='img/huaji.jpg'>"+"<span>"+"搞笑段子"+"</span>"+"</h1>"+"<p>"+t[i].group.content+"</p>"+"<div class='dv'>"+"<ul>"+"<li>"+"<span>"+"</span>"+"<p>"+"0"+"</p>"+"</li>"+"<li>"+"<span>"+"</span>"+"<p>"+"0"+"</p>"+"<li>"+"<span>"+"</span>"+"<p>"+"0"+"</p>"+"<ul>"+"<dv>"+"</li>"
+	}
 	$.ajax({
 		url:attr,
 		type:"get",
-		dataType:"jsonp",
-		jsonpCallback:"callback",
+		dataType:"json",
 		success:function(data)
 		{
 			for(var i=0;i<data.data.data.length;i++)
@@ -22,34 +28,26 @@ function ajax(attr,t)
 				var ul=document.getElementById("ul");
 				for(var i=0;i<t.length;i++)
 				{
-					ul.innerHTML+="<li>"+"<h1>"+"<img src='img/huaji.jpg'>"+"<span>"+"搞笑段子"+"</span>"+"</h1>"+"<p>"+t[i].group.content+"</p>"+"<div class='dv'>"+"<ul>"+"<li>"+"<span>"+"</span>"+"<p>"+"0"+"</p>"+"</li>"+"<li>"+"<span>"+"</span>"+"<p>"+"0"+"</p>"+"<li>"+"<span>"+"</span>"+"<p>"+"0"+"</p>"+"<ul>"+"<dv>"+"</li>"
+					xr(i);
 				}
 				var s=document.getElementById("s");
-				var x=0;
-				var y=0;
-				var sx=0;
-				var xy=0;
-				var top=0;
 				var ultop=ul.offsetTop;
 				s.addEventListener("touchstart",function(ev)
 				{
 					y=ev.changedTouches[0].pageY;
 					top=ul.offsetTop;
-					console.log(top);
-					console.log(ultop);
 				})
 				
 				s.addEventListener("touchmove",function(ev)
 				{
-					var a=y-ev.changedTouches[0].pageY;
-					if(top>=ultop && a<0)
+					var my=ev.changedTouches[0].pageY;
+					if(top>=ultop && my-y<0)
 					{
-						return;
+						ul.style.top=ultop+"px";
 					}
-					ul.style.top=-(a)+top+"px";
-				
+					ul.style.top=-(my-y)+top+"px";
 				})	
-}
+		}
 	})
 }
 //ajax(tp,TpArr);
